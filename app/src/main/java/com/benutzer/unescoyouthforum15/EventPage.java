faba896b07bb0c385bf68c8fbc41561bad7e137b
V-RaiD <rai.amitesh@ymail.com>
2015-07-14 14:36:57 +0530

    Second Commit - UYF15 App

--- app/src/main/java/com/benutzer/unescoyouthforum15/EventPage.java
+++ app/src/main/java/com/benutzer/unescoyouthforum15/EventPage.java
@@ -1,28 +1,125 @@
 package com.benutzer.unescoyouthforum15;
 
+import android.content.Intent;
+import android.media.Image;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 
 public class EventPage extends ActionBarActivity {
+
+    Handler handleEventData = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            displayEventData();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_page);
         loadEventData();
-
     }
 
     private void loadEventData(){
         Runnable runnable = new Runnable() {
             @Override
             public void run() {
-                
+                /*
+                code to load event image and information from site
+                 */
+                handleEventData.sendEmptyMessage(0);
             }
         };
+    }
+
+    private void displayEventData(){
+        ImageView eventImage = (ImageView) findViewById(R.id.eventPageImageId);
+        ScrollView eventTextView = (ScrollView) findViewById(R.id.scrollDescViewId);
+        String eventDescription = new String();
+
+        designEventDescString(eventTextView);
+        eventImage.setImageResource(R.drawable.splashimage2);
+    }
+
+    private void designEventDescString(ScrollView scrollView){
+        //TextView textView = new TextView(this);
+        scrollView.addView(new TextView(this){
+            @Override
+            public void setText(CharSequence text, BufferType type) {
+                super.setText("Name : " + getResources().getString(R.string.eventName));
+            }
+        });
+        scrollView.addView(new TextView(this){
+            @Override
+            public void setText(CharSequence text, BufferType type) {
+                super.setText("Timing : " + getResources().getString(R.string.eventTime));
+            }
+        });
+        scrollView.addView(new TextView(this){
+            @Override
+            public void setText(CharSequence text, BufferType type) {
+                super.setText("Venue : " + getResources().getString(R.string.eventLocationVenue));
+            }
+        });
+        scrollView.addView(new TextView(this){
+            @Override
+            public void setText(CharSequence text, BufferType type) {
+                super.setText("Street : " + getResources().getString(R.string.eventLocationStreet));
+            }
+        });
+        scrollView.addView(new TextView(this){
+            @Override
+            public void setText(CharSequence text, BufferType type) {
+                super.setText("City : " + getResources().getString(R.string.eventLocationCity));
+            }
+        });
+        scrollView.addView(new TextView(this){
+            @Override
+            public void setText(CharSequence text, BufferType type) {
+                super.setText("Pin : " + getResources().getString(R.string.eventLocationPin));
+            }
+        });
+        scrollView.addView(new TextView(this){
+            @Override
+            public void setText(CharSequence text, BufferType type) {
+                super.setText("Summary : " + getResources().getString(R.string.eventSummary));
+            }
+        });
+
+    }
+
+    public void speakerListGenerator(View view){
+        int eventId = 1;
+        Intent intent = new Intent(EventSpeakerPage.class);
+
+        /*
+        code to retrieve the event id to be passed to new activity
+         */
+        intent.putExtra("eventId", eventId);
+
+        startActivity(intent);
+    }
+
+    public void directionMapGenerator(View view){
+        int eventId = 1; //would not be hardcoded
+        Intent intent = new Intent(EventMapPage.class);
+
+        /*
+        code to retrieve the event id to be passed to new activity
+         */
+        intent.putExtra("eventId", eventId);
+
+        startActivity(intent);
     }
 
     @Override
